cc44c3ccba0a9864ac4e046ef3fedd9fde30b5f3
André Mion <andremion@gmail.com>
2018-11-30 16:35:32 +0000

    Update master (#26)
    * Upgrade build gradle tools to 3.1.4
    
    * Add badge position feature. (#18)
    
    * Feature/add text color attribute (#23)
    
    * Add badge text color attribute
    
    * Upgrade gradle build tools version to 3.2.1
    
    * Fix usage of SavedState for API < 24
    
    * Bump to version 1.2.0

--- counterfab/src/main/java/com/andremion/counterfab/CounterFab.java
+++ counterfab/src/main/java/com/andremion/counterfab/CounterFab.java
@@ -28,6 +28,7 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -338,7 +339,11 @@ public class CounterFab extends FloatingActionButton {
 
             @Override
             public SavedState createFromParcel(Parcel in, ClassLoader loader) {
-                return new SavedState(in, loader);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    return new SavedState(in, loader);
+                } else {
+                    return new SavedState(in);
+                }
             }
 
             @Override
