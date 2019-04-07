3d0d19bf5153fbca6bad043355ba94dd03a1c6d4
André Mion <andremion@gmail.com>
2019-04-07 21:15:19 +0100

    Refactoring/convert main source and tests to Kotlin (#32)
    * Reuse super state instead of instantiate a new one during the saving instance
    
    * Initial project setup to Kotlin
    
    * Convert main source to kotlin
    
    * Convert tests to Kotlin and add more cases for size mini
    
    * Fix calculation of circle bounds to consider size changed programmatically
    
    * Update tests and screenshots
    
    * Update dependencies and properties to publish into Bintray

---
+++ sample/src/androidTest/java/com/andremion/counterfab/sample/MainActivityTest.kt
@@ -0,0 +1,60 @@
+package com.andremion.counterfab.sample
+
+import android.content.pm.ActivityInfo
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.andremion.counterfab.sample.helpers.setFabSize
+import com.facebook.testing.screenshot.Screenshot
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+
+    @get:Rule
+    val testRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun shouldRenderProperly() {
+
+        Screenshot.snapActivity(testRule.activity).record()
+    }
+
+    @Test
+    fun shouldRenderProperlyAfterOrientationChanged() {
+
+        testRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+
+        Screenshot.snapActivity(testRule.activity).record()
+    }
+
+    @Test
+    fun shouldKeepStateAfterOrientationChanged() {
+        onView(withId(R.id.fab)).perform(click(), click())
+
+        testRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+
+        Screenshot.snapActivity(testRule.activity).record()
+    }
+
+    @Test
+    fun shouldRenderSizeMiniProperly() {
+        onView(withId(R.id.fab)).perform(setFabSize(FloatingActionButton.SIZE_MINI))
+
+        Screenshot.snapActivity(testRule.activity).record()
+    }
+
+    @Test
+    fun shouldRenderSizeMiniWithCountProperly() {
+        onView(withId(R.id.fab)).perform(
+                setFabSize(FloatingActionButton.SIZE_MINI), click(), click()
+        )
+
+        Screenshot.snapActivity(testRule.activity).record()
+    }
+}
